2ba457186aa03f7eaf47592e33b7d278e3be8dbc
C S MANODHITHYAA <manodhithyaa.cs@gmail.com>
2025-06-07 09:31:27 +0530

    changes in db.sql

--- utils/db.js
+++ utils/db.js
@@ -1,27 +1,65 @@
-require("dotenv").config();
-const mysql = require("mysql");
+const db = require("../db");
 
-const conn = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// Login Details
+function createLogin(data, callback) {
+  const sql = "INSERT INTO login_details SET ?";
+  db.query(sql, data, callback);
+}
 
-conn.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL Database!");
-});
+function getAllLogins(callback) {
+  db.query("SELECT * FROM login_details", callback);
+}
 
-module.exports = conn;
+// Vendors
+function createVendor(data, callback) {
+  const sql = "INSERT INTO vendors SET ?";
+  db.query(sql, data, callback);
+}
 
-/*
+function getAllVendors(callback) {
+  db.query("SELECT * FROM vendors", callback);
+}
 
-const db = require("./db");
+// Customers
+function createCustomer(data, callback) {
+  const sql = "INSERT INTO customers SET ?";
+  db.query(sql, data, callback);
+}
 
-db.query("SELECT * FROM users", (err, results) => {
-  if (err) throw err;
-  console.log(results);
-});
+function getAllCustomers(callback) {
+  db.query("SELECT * FROM customers", callback);
+}
 
-*/
+// Purchases
+function createPurchase(data, callback) {
+  const sql = "INSERT INTO purchases SET ?";
+  db.query(sql, data, callback);
+}
+
+function getAllPurchases(callback) {
+  db.query("SELECT * FROM purchases", callback);
+}
+
+// Purchase Items (Order)
+function addPurchaseItem(data, callback) {
+  const sql = "INSERT INTO purchase_items SET ?";
+  db.query(sql, data, callback);
+}
+
+function getItemsByPurchase(purchaseId, callback) {
+  const sql = "SELECT * FROM purchase_items WHERE purchase_id = ?";
+  db.query(sql, [purchaseId], callback);
+}
+
+module.exports = {
+  createLogin,
+  getAllLogins,
+  createVendor,
+  getAllVendors,
+  createCustomer,
+  getAllCustomers,
+  createPurchase,
+  getAllPurchases,
+  addPurchaseItem,
+  getItemsByPurchase,
+};
